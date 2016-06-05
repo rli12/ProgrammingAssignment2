3c0a128fa9d627bb6d36c19fa2e31801fed556ff
rli12 <RyanHLi42@gmail.com>
2016-06-05 18:42:02 -0400

    Update ProgrammingAssignment2.R

--- ProgrammingAssignment2.R
+++ ProgrammingAssignment2.R
@@ -1,9 +1,13 @@
 makeCacheMatrix <- function(x = matrix()){
+        #this function is created and then used to input into cacheSolve function
+        #inverse is initated to NULL
         inv <- NULL
         set <- function(y){
+                #both x and inv are assigned to an object different from current environment
                 x <<- y
                 inv <<- NULL
         }
+        
         get <- function() x
         setinverse <- function(inverse) inv <<- inverse
         getinverse <- function() inv
@@ -11,13 +15,16 @@ makeCacheMatrix <- function(x = matrix()){
 }
 
 cacheSolve <- function(x, ...){
+        #this returns the inverse of the original matrix
         inv <- x$getinverse()
         
+        #checks to see if the inverse is already calculated
         if(!is.null(inv)){
+                #if calculated run this
                 message("getting cached data")
                 return(inv)
         }
-        
+        #calculates inverse if it is not already calculated
         data <- x$get()
         inv <- solve(data, ...)
         x$setinverse(inv)
